38b589ff6e52fd99cbab9ac6716520910e0bfef5
MonadWizard <monad.wizard.r@gmail.com>
2021-06-03 00:31:00 +0600

    view basic product from fakeAPI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { useQuery } from 'react-query'
+import Drawer from '@material-ui/core/Drawer'
+// Components
+import Item from './Item/Item'
+import LinearProgress from '@material-ui/core/LinearProgress'
+import Grid from '@material-ui/core/Grid'
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
+import Badge from '@material-ui/core/Badge'
+// Styles
+import { Wrapper } from './App.styles'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export type CartItemType = {
+    id: number
+    category: string
+    description: string
+    image: string
+    price: number
+    title: string
+    amount: number
 }
 
-export default App;
+const getProducts = async (): Promise<CartItemType[]> =>
+    await (await fetch('https://fakestoreapi.com/products')).json()
+
+const App = () => {
+    const { data, isLoading, error } = useQuery<CartItemType[]>(
+        'products',
+        getProducts
+    )
+
+    console.log(data)
+
+    const getTotalItems = () => null
+
+    const handleAddToCart = () => null
+
+    const handleRemoveFromCart = () => null
+
+    if (isLoading) return <LinearProgress />
+    if (error) return <div> Something went wrong....... </div>
+
+    return (
+        <Wrapper>
+            <Grid container spacing={3}>
+                {data?.map((item) => (
+                    <Grid item key={item.id} xs={12} sm={4}>
+                        <Item item={item} handleAddToCart={handleAddToCart} />
+                    </Grid>
+                ))}
+            </Grid>
+        </Wrapper>
+    )
+}
+
+export default App
